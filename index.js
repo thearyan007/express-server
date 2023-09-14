bdbe3a632306ecfb207b82c30667fccd607baffc
thearyan007 <thearyangupta007@gmail.com>
2023-09-14 13:25:41 +0530

    get request

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res)=>{
+    res.send("Hello from the get!")
+})
+
 app.listen(port, ()=>{
     console.log("server is running in port "+port)
 })
